cc9b951d9b8d9935fb3184809749d4d7d82c5c47
Shardul Nalegave <shardul.nalegave.2005@gmail.com>
2024-02-06 20:38:38 +0530

    Added google analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,11 @@
+import ReactGA from 'react-ga4';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 import './index.css';
 import { router } from './router';
 
+ReactGA.initialize('G-LWQC66KG0Z');
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <RouterProvider router={router} />
