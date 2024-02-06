36f357ddd1d72b4baa1254bf7854d1761a82b756
Shardul Nalegave <shardul.nalegave.2005@gmail.com>
2024-02-06 17:46:43 +0530

    Responsiveness

--- src/components/page.tsx
+++ src/components/page.tsx
@@ -2,7 +2,8 @@ import { ReactNode } from 'react';
 
 export default function Page({ children } : { children: ReactNode }) {
   return (
-    <div className="h-screen overflow-y-scroll p-[50px]">
+    <div className="h-screen overflow-y-scroll p-[30px] lg:p-[50px]">
+      <div className='h-[80px] lg:h-[0px]'></div>
       { children }
     </div>
   );
--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,12 +1,42 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { IconMenu2, IconX } from '@tabler/icons-react';
 
 export default function Sidebar() {
+  const [showDrawer, setShowDrawer] = useState(false);
+
   return (
-    <div className='h-screen bg-zinc-900 text-white flex flex-col py-[30px] pl-[30px] pr-[75px]'>
-      <div className="grow"></div>
-      <div className="flex-none">
-        <Link to='/' className="text-5xl font-display hover:underline cursor-pointer">Shardul<br />Nalegave</Link>
+    <>
+      <div className='hidden lg:block'>
+        <div className='h-screen bg-zinc-900 text-white flex flex-col py-[30px] pl-[30px] pr-[75px]'>
+          <div className="grow"></div>
+          <div className="flex-none">
+            <Link to='/' className="text-5xl font-display hover:underline cursor-pointer">Shardul<br />Nalegave</Link>
+          </div>
+        </div>
       </div>
-    </div>
+
+      <div className='block lg:hidden'>
+        <div className='fixed top-0 left-0 right-0 p-[15px] bg-zinc-900 text-white'>
+          <div className="flex items-center">
+            <div className="flex-none">
+              <Link to='/' className='font-display text-xl'>Shardul Nalegave</Link>
+            </div>
+            <div className="grow"></div>
+            <div className="flex-none cursor-pointer" onClick={() => setShowDrawer(true)}>
+              <IconMenu2 />
+            </div>
+          </div>
+        </div>
+
+        <div className={`fixed top-0 right-0 left-0 bottom-0 overflow-x-hidden overlow-y-scroll bg-zinc-900 text-white p-[30px] ${showDrawer ? 'block' : 'hidden'}`}>
+          <div className='fixed top-0 right-0 p-[15px] cursor-pointer' onClick={() => setShowDrawer(false)}>
+            <IconX />
+          </div>
+          <div className='h-[30px]'></div>
+          <Link to='/' className="text-2xl font-display hover:underline cursor-pointer underline">Shardul Nalegave</Link>
+        </div>
+      </div>
+    </>
   );
 }
